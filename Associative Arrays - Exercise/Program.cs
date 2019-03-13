052276de799246945d79b0b3e9a796d1d56d556d
Velin Arbov <48315855+VelinArbov@users.noreply.github.com>
2019-03-13 15:22:03 +0200

    Add files via upload

---
+++ Associative Arrays - Exercise/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2.A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minersInfo = new Dictionary<string, int>();
+            
+            while (true)
+            {
+
+                string product = Console.ReadLine();
+                if (product == "stop")
+                {
+                    break;
+                }
+                int numbers = int.Parse(Console.ReadLine());
+               
+
+                if ( !minersInfo.ContainsKey(product))
+                {
+                    minersInfo[product] = numbers;
+                   
+                }
+                else
+                {
+                    minersInfo[product] += numbers;
+                }
+              
+
+            }
+            foreach (var key in minersInfo)
+            {
+
+
+                Console.WriteLine($"{key.Key} -> {string.Join(", ", key.Value)}");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
